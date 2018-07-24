71738089672152bb2badcedd9b6670a1e7cefdac
Michael Manlulu <michael.manlulu@tooltwist.com>
2018-07-24 12:22:19 +0800

    - created dashboard page responsive view

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,13 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  constructor(private renderer: Renderer2) {}
+
   showMyBuildings: boolean = false;
+  showMenu: boolean = false;
 
   ngOnInit() {
+    
+  }
+
+  pageUnscrollable(el) {
+    // Make page unscrollable
+    if(!el) {
+      this.renderer.removeClass(document.body, 'unscrollable');
+    } else { // Used conditions because renderer doesn't have toggleClass method
+      this.renderer.addClass(document.body, 'unscrollable');
+    }
   }
 
   toggleMyBuildings() {
     this.showMyBuildings = !this.showMyBuildings;
+    this.showMenu = false;
+    this.pageUnscrollable(this.showMyBuildings);
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+    this.showMyBuildings = false;
+    this.pageUnscrollable(this.showMenu);
   }
 
 }
